76ad039f4210ffbb06c25f32dde362e649af165f
Sam Howes <showes06@gmail.com>
2014-01-31 22:56:43 -0500

    Added Xcode Project for Native Code

---
+++ ARMonopolyNativeCodeBase/ARMonopolyNativeCodeBase/ARMViewController.h
@@ -0,0 +1,13 @@
+//
+//  ARMViewController.h
+//  ARMonopolyNativeCodeBase
+//
+//  Created by Samuel Howes on 1/31/14.
+//  Copyright (c) 2014 Samuel Howes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ARMViewController : UIViewController
+
+@end
---
+++ ARMonopolyNativeCodeBase/ARMonopolyNativeCodeBase/ARMViewController.m
@@ -0,0 +1,29 @@
+//
+//  ARMViewController.m
+//  ARMonopolyNativeCodeBase
+//
+//  Created by Samuel Howes on 1/31/14.
+//  Copyright (c) 2014 Samuel Howes. All rights reserved.
+//
+
+#import "ARMViewController.h"
+
+@interface ARMViewController ()
+
+@end
+
+@implementation ARMViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
