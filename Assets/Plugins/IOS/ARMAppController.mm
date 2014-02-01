2b14763096337c75bad8dd10bf424632bcdb0f9d
Sam Howes <showes06@gmail.com>
2014-02-01 01:47:42 -0500

    Proof of Concept
    Got the basic setup for the Native UI working:
    - App loads into a custom storyboard
    - Storyboard allows navigation to the Unity view, and back to the home
    screen of the app.

---
+++ Assets/Plugins/IOS/ARMAppController.mm
@@ -0,0 +1,19 @@
+﻿#import <UIKit/UIKit.h>
+#import "UnityAppController.h"
+#import "UI/UnityView.h"
+
+@interface ARMAppController : UnityAppController
+{
+}
+- (void)createViewHierarchyImpl;
+@end
+@implementation ARMAppController
+- (void)createViewHierarchyImpl;
+{
+	_rootController = [[ARMViewController alloc] init];
+	_rootView = _unityView;
+}
+@end
+
+IMPL_APP_CONTROLLER_SUBCLASS(ARMAppController)
+
